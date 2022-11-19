24f65cc759b2b76e186834a859c7c2a1b842e9ce
Sheb95 <s_saund@outlook.com>
2022-11-19 23:22:48 +0000

    successfully deployed

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg_name" {
-  name     = "sheb"
+  name     = var.resource_group_name
   location = "ukwest"
 }
 
@@ -24,3 +24,58 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "ukwest"
   resource_group_name = azurerm_resource_group.rg_name.name
 }
+
+resource "azurerm_subnet" "my_subnet"{
+  name = "subnet-1"
+  resource_group_name = azurerm_resource_group.rg_name.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.0.0/12"]
+}
+
+resource "azurerm_network_interface" "nic"{
+  name = "sheb-nic"
+  location = azurerm_resource_group.rg_name.location
+  resource_group_name = azurerm_resource_group.rg_name.name
+
+  ip_configuration {
+    name = "subnet-1"
+    subnet_id = azurerm_subnet.my_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+}
+
+resource "azurerm_linux_virtual_machine" "linux_VM" {
+  name                = "my-machine"
+  resource_group_name = azurerm_resource_group.rg_name.name
+  location            = azurerm_resource_group.rg_name.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password = "P@ssword3!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+
+
+
+
+
+
+
+
